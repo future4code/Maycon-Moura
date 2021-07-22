5c489f4f7960cadafc5e0bcc2fd68207147ee736
MayconSanches <68667157+MayconSanches@users.noreply.github.com>
2021-07-22 19:57:48 -0300

    alteração minha-bio

---
+++ template_javascript/index.js
@@ -0,0 +1,30 @@
+//Exercicio 1: Vai mostrar 10, 10 5
+//exercicio 2: 10, 10, 10
+
+//Exercicio 3: 
+//let quantasHoras = prompt("Quantas horas você trabalha por dia?")
+//let quantoRecebe = prompt("Quanto você recebe por dia?")
+//alert(`Voce recebe ${t/p} por hora`)
+
+//let nome
+//let idade
+
+//console.log(typeof nome)
+// undefined: ele fala que a variavel é indefinida
+const nome = prompt("Qual seu nome?")
+const idade = prompt("Qual a sua idade?")
+
+//console.log(nome)
+//console.log(idade)
+
+console.log("Olá", nome, "você tem,", idade, "anos")
+
+const bolo = prompt ("Você gosta de bolo?")
+console.log ("Você gosta de bolo?", bolo)
+
+const jogos = prompt ("Você joga video-game?")
+console.log ("Você joga video-game?", jogos)
+
+const estudo = prompt ("Você gosta de estudar?")
+console.log ("Você gosta de estudar?", estudo)
+
