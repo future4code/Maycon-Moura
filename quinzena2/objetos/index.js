9b99f9b073b8ae74bb886ce8cab5d46a85a80ae5
MayconSanches <68667157+MayconSanches@users.noreply.github.com>
2021-07-29 21:03:50 -0300

    Aula 7 objetos

---
+++ quinzena2/objetos/index.js
@@ -0,0 +1,57 @@
+// const filme = {
+// 	nome: "Auto da Compadecida", 
+// 	ano: 2000, 
+// 	elenco: [
+// 		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
+// 		"Virginia Cavendish"
+// 		], 
+// 	transmissoesHoje: [
+// 		{canal: "Telecine", horario: "21h"}, 
+// 		{canal: "Canal Brasil", horario: "19h"}, 
+// 		{canal: "Globo", horario: "14h"}
+// 		]
+// }
+
+// console.log(filme.elenco[0])
+// console.log(filme.elenco[filme.elenco.length - 1])
+// console.log(filme.transmissoesHoje[2])
+
+// Matheus Nachtergaele
+// Virginia Cavendish
+// Objectcanal: "Globo"horario: "14h"[[Prototype]]: Object
+
+// const cachorro = {
+// 	nome: "Juca", 
+// 	idade: 3, 
+// 	raca: "SRD"
+// }
+
+// const gato = {...cachorro, nome: "Juba"}
+
+// const tartaruga = {...gato, nome: gato.nome.replaceAll("a", "o")}
+
+// console.log(cachorro)
+// console.log(gato)
+// console.log(tartaruga)
+
+//a-) O nome de cada String
+//b-)Abrevia o código
+
+
+// const nomeString = {
+//     nome: "Amanda",
+
+//     Apelidos: ["Amandinha","Mandinha", "Mandi"]
+
+// }
+
+// console.log("Me chamo", nomeString.nome, "mas pode me chamar de:", nomeString.Apelidos)
+
+
+
+
+
+
+
+
+
